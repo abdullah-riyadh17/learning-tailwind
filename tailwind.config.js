46380efdec031b9a888afad033adfca7c2f92631
Abdullah Riyadh <abdullahriyadhcse@gmail.com>
2023-05-17 13:13:42 +0600

    Dark variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./pages/**/*.{html,js}",
     "./index.html",
   ],
+  darkMode: "media", // meadia or class ; media is device preferences(based on theme), class is option to the user to select them.
   theme: {
     extend: {},
   },
